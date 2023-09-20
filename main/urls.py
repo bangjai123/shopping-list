326e4b2bfcd835d79b0753a2a7320497d1b987b4
bangjai123 <znaufalilmi@gmail.com>
2023-09-20 15:16:48 +0700

    Mengumpulkan

--- main/urls.py
+++ main/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 from main.views import show_main, create_product, show_xml, show_json
-
+from main.views import register
+from main.views import logout_user
+from main.views import login_user
 app_name = 'main'
 
 urlpatterns = [
-    
+    path('logout/', logout_user, name='logout'),
     path('xml/', show_xml, name='show_xml'), 
     path('create-product', create_product, name='create_product'),
     path('', show_main, name='show_main'),
-    path('json/', show_json, name='show_json'), 
+    path('json/', show_json, name='show_json'),
+    path('register/', register, name='register'), 
+    path('login/', login_user, name='login'),
 ]
--- shopping_list/urls.py
+++ shopping_list/urls.py
@@ -18,12 +18,12 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import path, include
 from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id 
-
+from main.views import login_user
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls')),
+    path('', include('main.urls', namespace='main')),
     path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<int:id>/', show_json_by_id, name='show_json_by_id'),
-    
+    path('login/', login_user, name='login') 
 ]
