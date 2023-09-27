f5bc4d62ac51f431f70a6404df26a212366b7edc
bangjai123 <znaufalilmi@gmail.com>
2023-09-27 15:18:55 +0700

    selesai tutorial

--- main/urls.py
+++ main/urls.py
@@ -3,9 +3,12 @@ from main.views import show_main, create_product, show_xml, show_json
 from main.views import register
 from main.views import logout_user
 from main.views import login_user
+from main.views import edit_product
+from main.views import delete_product
 app_name = 'main'
 
 urlpatterns = [
+    path('delete/<int:id>', delete_product, name='delete_product'),
     path('logout/', logout_user, name='logout'),
     path('xml/', show_xml, name='show_xml'), 
     path('create-product', create_product, name='create_product'),
@@ -13,4 +16,5 @@ urlpatterns = [
     path('json/', show_json, name='show_json'),
     path('register/', register, name='register'), 
     path('login/', login_user, name='login'),
+    path('edit-product/<int:id>', edit_product, name='edit_product'),
 ]
